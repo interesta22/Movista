c9725e8706f223ff0364380807d7949939db06fb
interesta22 <2albelasd@gmail.com>
2025-08-07 16:31:23 +0300

    Create Home Repo Methods and Failures Class

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,5 @@
+abstract class Failures {}
+
+class NetworkFailure extends Failures {}
+class ServerFailure extends Failures {}
+class CacheFailure extends Failures {}
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,9 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:movista_app/core/errors/failures.dart';
 import 'package:movista_app/features/home/data/models/movie_model.dart';
 
 abstract class HomeRepo {
-  Future<List<MovieModel>> fetchDiscoverMovies();
-  Future<List<MovieModel>> fetchPopularMovies();
-  Future<List<MovieModel>> fetchTopRatedMovies();
-  Future<List<MovieModel>> fetchcomingMovies();
-  Future<List<MovieModel>> fetchNowPlayingMovies();
+  Future<Either<Failures, List<MovieModel>>> fetchDiscoverMovies();
+  Future<Either<Failures, List<MovieModel>>> fetchPopularMovies();
+  Future<Either<Failures, List<MovieModel>>> fetchTopRatedMovies();
+  Future<Either<Failures, List<MovieModel>>> fetchcomingMovies();
+  Future<Either<Failures, List<MovieModel>>> fetchNowPlayingMovies();
 }
